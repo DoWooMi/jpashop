b75670aea29ef0b53936a13430c7fe8f6d2c879b
DoWooMi <kjk2875@gmail.com>
2023-04-28 00:04:18 +0900

    [create] MemberRepository class

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository//스프링 빈으로 등록
+public class MemberRepository {
+
+    //스프링이 entitymanager 빈으로 만들어서 주입
+    @PersistenceContext
+    private EntityManager em;
+
+    //JPA가 entity를 저장하는 로직
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    //JPA가 제공하는 find메소드로 id값을 넘기면 member찾아서 반환
+    public Member findOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    //List 조회 (JPQL을 실행하기 위한 쿼리객체 생성 후 결과 반환)
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    //이름으로 회원을 검색하여 List 조회
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
