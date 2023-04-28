1bc6d7b41ea77ea7fc2827188cc99a58b2f0204f
DoWooMi <kjk2875@gmail.com>
2023-04-28 11:24:47 +0900

    [create] MemberService class

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor//final이 있는 필드만을 가지고 생성자 만듦
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //회원 가입
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();//값이 있다는 것이 보장됨(id가 키값)
+    }
+
+    private void validateDuplicateMember(Member member){
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    //id에 해당하는 회원 조회(단건조회)
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
