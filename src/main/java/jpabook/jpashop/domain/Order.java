47533624914a3b69cbd3b1b58c6823eb21af5582
DoWooMi <kjk2875@gmail.com>
2023-04-30 18:24:37 +0900

    [update] Order Entity

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -9,6 +12,7 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Table(name = "orders")
+@Getter @Setter
 public class Order {
 
     @Id @GeneratedValue
@@ -32,19 +36,56 @@ public class Order {
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
 
     // == 연관관계 메서드 == //
+    //member와 order : 1:N 관계
     public void setMember(Member member){
         this.member=member;
         member.getOrders().add(this);
     }
 
+    //orderitem과 order : N:1 관계
     public void addOrderItem(OrderItem orderItem){
         orderItems.add(orderItem);
         orderItem.setOrder(this);
     }
 
+    //delivery와 order : 1:1 관계
     public void setDelivery(Delivery delivery){
         this.delivery=delivery;
         delivery.setOrder(this);
     }
 
+    // == 생성 메서드 (주문생성) == //
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);//주문상태 : ORDER
+        order.setOrderDate(LocalDateTime.now());//주문시간 : 현재시간
+        return order;
+    }
+
+    // == 비즈니스 로직 == //
+    //주문 취소
+    public void cancel(){
+        if(delivery.getStatus()==DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);//주문상태 : CANCEL
+        for (OrderItem orderItem: orderItems) {
+            orderItem.cancel();//재고수량 증가
+        }
+    }
+
+    // == 조회 로직 == //
+    //전체 주문 가격 조회
+    public int getTotalPrice(){
+        int totalPrice = 0;
+        for (OrderItem orderItem: orderItems) {
+            totalPrice += orderItem.getTotalPrice();//주문가격과 주문수량이 필요하므로 orderitem메소드 호출
+        }
+        return totalPrice;
+    }
 }
