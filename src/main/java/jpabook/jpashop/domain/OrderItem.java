8f962032470fb96f902f5a58b2a0cd2ed59d1a8a
DoWooMi <kjk2875@gmail.com>
2023-04-30 18:24:56 +0900

    [update] OrderItem Entity

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -26,4 +26,27 @@ public class OrderItem {
 
     private int orderPrice; //주문 가격
     private int count; //주문 수량
+
+    // == 생성 메서드 (주문아이템 생성) == //
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){//쿠폰받거나 할인받을 수 있기 때문에 orderprice 따로 작성
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        
+        item.removeStock(count);
+        return orderItem;
+    }
+    
+    // == 비즈니스 로직 == //
+    //주문취소시 재고수량 원복
+    public void cancel(){
+        getItem().addStock(count);
+    }
+
+    // == 조회 로직 == //
+    //주문상품 전체가격 조회
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
