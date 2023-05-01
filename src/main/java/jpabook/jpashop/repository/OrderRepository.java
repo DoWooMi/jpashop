16b91c59b32f5a8de023226ea7793453be6f8a24
DoWooMi <kjk2875@gmail.com>
2023-05-01 09:52:52 +0900

    [create] OrderRepository

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order){
+        em.persist(order);
+    }
+
+    public Order findOne(Long id){
+        return em.find(Order.class, id);
+    }
+
+    //검색결과 조회
+    /*
+    public List<Order> findAll(OrderSearch orderSearch){
+        return em.createQuery("select m from order m",Order.class).getResultList();
+    }
+     */
+}
